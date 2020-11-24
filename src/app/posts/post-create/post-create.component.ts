a6039db25fcafb4766005e8e8a0f19146e4ac94f
Shivam Agrawal <shivam96.sa@gmail.com>
2020-11-24 14:50:38 +0530

    Added Frontend of Application

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  enteredTitle = "";
+  enteredContent = "";
+
+  constructor(public postService: PostService) {}
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postService.addPost(form.value.title, form.value.content);
+    console.log(this.postService.getPosts())
+    form.resetForm();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
