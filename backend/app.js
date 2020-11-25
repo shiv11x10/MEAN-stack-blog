1925c959129c2a5056962530f9f61d950b86672a
Shivam Agrawal <shivam96.sa@gmail.com>
2020-11-25 12:09:55 +0530

    Added image upload functionality to posts

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -21,6 +22,7 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
